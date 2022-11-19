3525489db0a1f3fca83761a4ee8ad8635891c87d
PaulBDev <pythonkoder@gmail.com>
2022-11-19 13:05:52 -0600

    fix brave error

--- client/src/components/Wallet.js
+++ client/src/components/Wallet.js
@@ -97,8 +97,38 @@ export default function Wallet() {
       return;
     }
 
+    let isMobile = {
+      Android: function () {
+        return navigator.userAgent.match(/Android/i);
+      },
+      BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+      },
+      iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+      },
+      Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+      },
+      Windows: function () {
+        return (
+          navigator.userAgent.match(/IEMobile/i) ||
+          navigator.userAgent.match(/WPDesktop/i)
+        );
+      },
+      any: function () {
+        return (
+          isMobile.Android() ||
+          isMobile.BlackBerry() ||
+          isMobile.iOS() ||
+          isMobile.Opera() ||
+          isMobile.Windows()
+        );
+      },
+    };
+
     // Method for getting address on mobile only
-    if (navigator.userAgentData.mobile) {
+    if (isMobile.any()) {
       await connect()
         .then(async (res) => {
           let address = res[0];
